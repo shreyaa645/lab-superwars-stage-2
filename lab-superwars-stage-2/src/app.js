087453d3d3804b50f5f3cc569e007326dbfa3c19
Shreya Pawar <144438450+shreyaa645@users.noreply.github.com>
2024-02-14 18:24:30 +0530

    Add files via upload

---
+++ lab-superwars-stage-2/src/app.js
@@ -0,0 +1,78 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+    // Create players using for loop
+    // Type your code here
+    for (let i = 0; i < players.length; i++) {
+        detailedPlayers.push({
+            name: players[i],
+            strength: getRandomStrength(),
+            image: `images/super-${i + 1}.png`,
+            type: i % 2 === 0 ? "hero" : "villain" 
+        });
+    }
+    return detailedPlayers;
+}
+
+// getting random strength
+const getRandomStrength = () => {
+    // Return a random integer (0,100]
+    // Note: You can use Math.random() and Math.ceil()
+    let ranNum = Math.floor((Math.random()*100)+1)
+    return ranNum;
+}
+
+const buildPlayers = (players, type) => {
+    let fragment = '';
+
+    // Loop through players and accumulate HTML template
+    // depending of type of player(hero|villain)
+    // Type your code here
+    for (let i = 0; i < players.length; i++) {
+        if(players[i].type== type)
+        {
+            let text = `<div class="player">
+            <img src="${players[i].image}" alt="">
+            <div class="name">${players[i].name}</div>
+            <div class="strength">${players[i].strength}</div>
+            </div>`
+    
+            fragment = fragment + text;
+        }}
+
+    return fragment;
+}
+// Display players in HTML
+const viewPlayers = (players) => {
+
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+
+}
+
+window.onload = () => {
+    viewPlayers(initPlayers(PLAYERS));
+}
